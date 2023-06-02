d7122f1c3913f6b1a9e6ce4e00ccec5bf552ef12
oberon4589 <oberon4589@gmail.com>
2023-06-02 10:11:52 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function App() {
+  const [todos, setTodos] = useState([
+    
+  ])
+
+  return (
+    <div>
+
+      Olá react
+
+    </div>
+      
+  )
+}
+
+export default App
