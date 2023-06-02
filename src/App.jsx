79e117bd75a358b17dd4472fa3fe30d17a70fd80
oberon4589 <oberon4589@gmail.com>
2023-06-02 10:48:21 -0300

    estilizção e criação de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
 import { useState } from 'react'
+import './App.css'
+import ToDo from './components/ToDo'
+import ToDoForm from './components/ToDoForm'
 
 function App() {
   const [todos, setTodos] = useState([
-    
+    {
+      id: 1,
+      text: 'Criar funcionalidade de adicionar tarefas',
+      category: 'Trabalho',
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: 'Ir a academia',
+      category: 'Pessoal',
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: 'Estudar React',
+      category: 'Estudos',
+      isCompleted: false,
+    },
   ])
 
   return (
-    <div>
-
-      Olá react
-
+    <div className='app'>
+      <h1>Lista de tarefas</h1>
+      <div className='todo-list'>
+        {todos.map((todo) => (
+          <ToDo todo={todo}/>
+        ))}
+      </div>
+      <ToDoForm />
     </div>
       
   )
